9f37aa5af4ea8deae48b48613bdfb8e9d9d22fed
Danny-Parbat <parbatdanny26@gmail.com>
2025-11-30 19:16:41 +0545

    first commit

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+class Arrays{
+    public static void main(String[] args){
+        int a[]={10,20,12,16,5};
+        int max=a[0];
+        int min=a[0];
+        for(int i=1; i<a.length; i++){
+            if(max<a[i]){
+                max=a[i];
+            }
+            if(min>a[i]){
+                min=a[i];
+            }
+        }
+        System.out.println("min: "+min + "max: "+max);
+    }
+}
+
